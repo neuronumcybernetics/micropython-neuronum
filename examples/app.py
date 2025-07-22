3969867c632642f3c29c84bc33411f3a04d23d29
neuronumcybernetics <welcome@neuronum.net>
2025-07-22 13:00:20 +0200

    app.py exampel added

---
+++ examples/app.py
@@ -0,0 +1,34 @@
+import micropython_neuronum as neuronum
+
+cell = neuronum.Cell(
+    host="host",
+    password="password",
+    network="neuronum.net",
+    synapse="synapse"
+)
+     
+STX = "id::stx"
+for operation in cell.sync(STX):  
+    txID = operation.get("txID")
+    client = operation.get("operator")                    
+                            
+    if txID == "id::tx":             
+        data = {
+            "json": f"Hello {client}",
+            "html": f"""
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Greeting Node</title>
+  </head>
+  <body>
+    <div class="card">
+      <h1>Hello, {client}</h1>
+    </div>
+  </body>
+</html>
+"""
+        }
+        cell.tx_response(txID, client, data)
+
